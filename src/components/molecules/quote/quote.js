3574d240f70b1eed8eb4ffa2c4a08b6fea8b06cc
Moreno Feltscher <moreno@smartive.ch>
2018-05-30 15:29:17 +0200

    feat: Add template for quotes

--- src/components/molecules/quote/quote.js
+++ src/components/molecules/quote/quote.js
@@ -1,3 +1,47 @@
-export const Quote = () => null;
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './quote.scss';
+
+export const Quote = ({ text, author, img, company, url }) => (
+  <div className="container">
+    <div className="quote-block">
+      <div className="row">
+        <div className="col-xs-12 col-md-3">
+          <img
+            className="quote__img"
+            alt={`Portrait von ${author}`}
+            src={img}
+          />
+        </div>
+        <div className="col-xs-12.col-md-9 quote__body">
+          <blockquote className="quote">
+            <p>{text}</p>
+            <cite>
+              <span>
+                {author}
+                {company ? ', ' : null}
+              </span>
+              {url ? <a href={url}>{company}</a> : <span>{company}</span>}
+            </cite>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+Quote.propTypes = {
+  text: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  company: PropTypes.string,
+  url: PropTypes.string,
+};
+
+Quote.defaultProps = {
+  company: null,
+  url: null,
+};
 
 export default Quote;
