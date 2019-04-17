6263de5d45876f45aa83c738e6ba272e2ffda5dc
Thomas Moser <thomas@smartive.ch>
2019-04-17 14:33:37 +0000

    fix(): Fix external links on images

--- src/components/atoms/link-button/link-button.js
+++ src/components/atoms/link-button/link-button.js
@@ -1,27 +1,19 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
+import React from 'react';
 
 import './link-button.scss';
+import { LinkWrapper } from '../link-wrapper';
 
 const mod = (modifier) => ` link-button--${modifier}`;
 
 export const LinkButton = ({ url, text, isPrimary, isWhite, hasBorder }) => {
-  const internal = /^\/(?!\/)/.test(url);
   const className = `link-button${isPrimary ? mod('primary') : ''}${isWhite ? mod('white') : ''}${
     hasBorder ? ' has-border' : ''
   }`;
-  if (internal) {
-    return (
-      <Link to={url} className={className}>
-        {text}
-      </Link>
-    );
-  }
   return (
-    <a href={url} className={className}>
+    <LinkWrapper to={url} className={className}>
       {text}
-    </a>
+    </LinkWrapper>
   );
 };
 
