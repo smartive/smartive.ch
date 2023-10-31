bd792d10a9311cdfef791e15cf133170fc49ec2c
Kevin Walker <kevin@smartive.ch>
2023-10-31 08:47:14 +0100

    feat: add support for vimeo videos

--- components/dato/blocks/video.tsx
+++ components/dato/blocks/video.tsx
@@ -5,18 +5,32 @@ type Props = {
   block: VideoBlockFragment;
 };
 
-export const VideoBlock: FC<Props> = ({ block: { video } }) => {
-  // Currently only supports youtube videos
-  if (video?.provider !== 'youtube') {
-    return null;
+const getVideoSrc = (video: VideoBlockFragment['video'], loop: boolean, autoplay: boolean) => {
+  switch (video?.provider) {
+    case 'youtube':
+      return `https://www.youtube-nocookie.com/embed/${video.providerUid}?&controls=0&autoplay=${autoplay ? 1 : 0}&loop=${
+        loop ? 1 : 0
+      }`;
+    case 'vimeo':
+      return `https://player.vimeo.com/video/${video.providerUid}?autoplay=${autoplay ? 1 : 0}&muted=${
+        autoplay ? 1 : 0
+      }&loop=${loop ? 1 : 0}&title=0&byline=0&portrait=0&controls=0`;
+    default:
+      return;
   }
+};
+
+export const VideoBlock: FC<Props> = ({ block: { video, loop, autoplay } }) => {
+  const videoSrc = getVideoSrc(video, loop ?? false, autoplay ?? false);
+
+  if (!videoSrc) return null;
 
   return (
     <div className="my-12 lg:my-48">
       <iframe
         className="aspect-video w-full rounded"
         title={video?.title}
-        src={`https://www.youtube-nocookie.com/embed/${video.providerUid}?si=6XsfcGAo_1PfhP1B&amp;controls=0`}
+        src={videoSrc}
         allow="autoplay; fullscreen; picture-in-picture"
         allowFullScreen
       />
