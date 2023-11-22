7108da45cffd62280d24a48ab83e1459d31c0e60
Kevin Walker <kevin@smartive.ch>
2023-11-22 13:44:21 +0100

    fix: image grid layout "portrait-left"

--- components/dato/blocks/image-grid.tsx
+++ components/dato/blocks/image-grid.tsx
@@ -14,7 +14,7 @@ export const ImageGridBlock: FC<Props> = ({ block: { image1, image2, image3, lay
       {image1?.responsiveImage && (
         <DatoImage
           data={image1.responsiveImage}
-          className={classNames('rounded', layout === 'portrait-left' ? 'row-start-2' : 'row-start-1')}
+          className={classNames('row-start-1 rounded', layout === 'portrait-left' ? 'col-start-2' : 'col-start-1')}
         />
       )}
       {image2?.responsiveImage && (
@@ -26,7 +26,10 @@ export const ImageGridBlock: FC<Props> = ({ block: { image1, image2, image3, lay
       {image3?.responsiveImage && (
         <DatoImage
           data={image3.responsiveImage}
-          className={classNames('row-span-2 rounded', layout === 'portrait-left' ? 'col-start-1' : 'col-start-2')}
+          className={classNames(
+            'row-span-2 row-start-1 rounded',
+            layout === 'portrait-left' ? 'col-start-1' : 'col-start-2',
+          )}
         />
       )}
     </div>
