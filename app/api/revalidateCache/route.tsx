01ae7a064c3b501d84ec0fe7c0d4dc0e195281dd
Kevin Walker <kevin@smartive.ch>
2024-02-12 13:46:43 +0000

    feat: use revalidatePath

--- app/api/revalidateCache/route.tsx
+++ app/api/revalidateCache/route.tsx
@@ -1,6 +1,7 @@
 import { revalidatePath, revalidateTag } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 import { ALL_PAGES_TAG, PAGE_STRUCTURE_TAG } from '../../../utils/const';
+import { getAllDatoRoutes } from '../../../utils/get-dato-routes';
 
 type BodyType = {
   event_type: 'create' | 'update' | 'delete' | 'publish' | 'unpublish';
@@ -16,12 +17,21 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
   }
 
   const { entity_slug, event_type, item_type } = (await req.json()) as BodyType;
-
+  const paths: string[] = [];
   const tags: string[] = [];
 
-  // Add slug to tags, in order to invalidate the cache for the page
-  if (entity_slug) {
-    tags.push(entity_slug);
+  // find the route for the slug
+  const allRoutes = await getAllDatoRoutes();
+  const route = [
+    ...allRoutes.pages,
+    ...allRoutes.blogposts,
+    ...allRoutes.offers,
+    ...allRoutes.projects,
+    ...allRoutes.topics,
+  ].find((route) => route.path.includes(entity_slug));
+
+  if (route) {
+    paths.push(route.path);
   }
 
   // If a new page is created, invalidate the cache for all pages
@@ -31,22 +41,30 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
 
   // Revalidate overview pages
   if (item_type === 'blogpost') {
-    revalidatePath('/blog');
+    paths.push('/blog');
   }
 
   if (item_type === 'project') {
-    revalidatePath('/projekte');
+    paths.push('/projekte');
   }
 
   if (item_type === 'offer') {
-    revalidatePath('/angebot');
+    paths.push('/angebot');
   }
 
   if (item_type === 'employee') {
-    revalidatePath('/team');
+    paths.push('/team');
+  }
+
+  if (paths.length === 0 && tags.length === 0) {
+    return NextResponse.json({
+      status: 400,
+      body: { message: 'No paths or tags to revalidate' },
+    });
   }
 
   try {
+    paths.map((path) => revalidatePath(path));
     tags.map((tag) => revalidateTag(tag));
   } catch (error: unknown) {
     return NextResponse.json({
@@ -55,5 +73,5 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     });
   }
 
-  return NextResponse.json({ revalidated: true, now: Date.now(), tags });
+  return NextResponse.json({ revalidated: true, now: Date.now(), paths, tags });
 }
