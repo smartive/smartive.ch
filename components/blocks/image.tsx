e5ad26124c6adda9d1eea38991f9fe3d901f5135
Céline Salzmann <celine@smartive.ch>
2024-02-27 13:17:40 +0000

    feat: add priority prop to image block to preload images

--- components/blocks/image.tsx
+++ components/blocks/image.tsx
@@ -10,22 +10,23 @@ type Props = {
 type ImageProps = {
   image: ResponsiveImageType;
   caption?: string;
+  priority?: boolean;
 };
 
-const Image: FC<ImageProps> = ({ image, caption }) => (
+const Image: FC<ImageProps> = ({ image, caption, priority }) => (
   <div>
-    <DatoImage data={image} layout="responsive" className="rounded" />
+    <DatoImage data={image} layout="responsive" className="rounded" priority={priority} />
     {caption && <p className="mt-2 text-xs italic">{caption}</p>}
   </div>
 );
 
-export const ImageBlock: FC<Props> = ({ block: { images } }) => {
+export const ImageBlock: FC<Props> = ({ block: { images, priority } }) => {
   if (images.length === 1) {
     const { id, title, responsiveImage } = images[0];
 
     return (
       <BlockWrapper marginTop="small" marginBottom="small">
-        <Image key={id} image={responsiveImage} caption={title ? title : undefined} />
+        <Image key={id} image={responsiveImage} caption={title ?? undefined} priority={priority} />
       </BlockWrapper>
     );
   } else if (images.length > 1) {
@@ -34,7 +35,7 @@ export const ImageBlock: FC<Props> = ({ block: { images } }) => {
         <div className="grid gap-2 sm:auto-cols-[minmax(0,_1fr)] sm:grid-flow-col sm:gap-8 xl:gap-16">
           {images.map(
             ({ id, title, responsiveImage }) =>
-              responsiveImage && <Image key={id} image={responsiveImage} caption={title ? title : undefined} />,
+              responsiveImage && <Image key={id} image={responsiveImage} caption={title ?? undefined} priority={priority} />,
           )}
         </div>
       </BlockWrapper>
