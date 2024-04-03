651cbe06b9ef882246e385d2dcce3c4b4607e90f
Kevin Walker <kevin@smartive.ch>
2024-04-03 11:42:33 +0000

    feat: use vercel's git integration for deployments

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-export async function middleware(request: NextRequest) {
+
+export function middleware(request: NextRequest) {
   // remove home from pathname, because its prettier to have / instead of /home for the home page
   if (request.nextUrl.pathname === '/home') {
     return NextResponse.redirect(new URL(`/`, request.url));
