f39e482c272d96597fd9c47fafa547304bfeb16c
Kevin Walker <kevin@smartive.ch>
2024-04-10 11:27:09 +0200

    chore: add comment to /home redirect

--- next.config.js
+++ next.config.js
@@ -49,6 +49,8 @@ module.exports = withBundleAnalyzer({
         permanent: true,
       },
       {
+        // DatoCMS requires a slug for each page and we use "home" for the home page.
+        // We redirect it here, because its prettier to have / instead of /home for the home page.
         source: '/home',
         destination: '/',
         permanent: true,
