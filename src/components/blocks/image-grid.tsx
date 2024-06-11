6f16eabc826687f5f44e189cf4ac4625d58ec15c
Kevin Walker <kevin@smartive.ch>
2024-06-11 14:13:28 +0000

    fix: image grid

--- src/components/blocks/image-grid.tsx
+++ src/components/blocks/image-grid.tsx
@@ -1,4 +1,4 @@
-import { ImageGridBlockFragment } from '@/graphql/generated';
+import { ImageGridBlockFragment, ResponsiveImageFragment } from '@/graphql/generated';
 import { classNames } from '@smartive/guetzli';
 import { FC } from 'react';
 import { SRCImage as DatoSRCImage } from 'react-datocms';
@@ -8,31 +8,29 @@ type Props = {
   block: ImageGridBlockFragment;
 };
 
+const Image: FC<{ image: ResponsiveImageFragment }> = ({ image }) => (
+  <DatoSRCImage data={image} style={{ width: '100%', maxWidth: '100%', height: '100%' }} className="rounded object-cover" />
+);
+
 export const ImageGridBlock: FC<Props> = ({
   block: { image1, image2, image3, layout, disableMarginTop, disableMarginBottom },
 }) => (
   <BlockWrapper marginTop={disableMarginTop ? 'none' : 'large'} marginBottom={disableMarginBottom ? 'none' : 'large'}>
     <div className="flex flex-col gap-8 sm:grid sm:grid-cols-2 sm:grid-rows-2 xl:gap-16">
       {image1?.responsiveImage && (
-        <DatoSRCImage
-          data={image1.responsiveImage}
-          className={classNames('row-start-1 rounded', layout === 'portrait-left' ? 'col-start-2' : 'col-start-1')}
-        />
+        <div className={classNames('row-start-1', layout === 'portrait-left' ? 'col-start-2' : 'col-start-1')}>
+          <Image image={image1.responsiveImage} />
+        </div>
       )}
       {image2?.responsiveImage && (
-        <DatoSRCImage
-          data={image2.responsiveImage}
-          className={classNames('row-start-2 rounded', layout === 'portrait-left' ? 'col-start-2' : 'col-start-1')}
-        />
+        <div className={classNames('row-start-2', layout === 'portrait-left' ? 'col-start-2' : 'col-start-1')}>
+          <Image image={image2.responsiveImage} />
+        </div>
       )}
       {image3?.responsiveImage && (
-        <DatoSRCImage
-          data={image3.responsiveImage}
-          className={classNames(
-            'row-span-2 row-start-1 rounded',
-            layout === 'portrait-left' ? 'col-start-1' : 'col-start-2',
-          )}
-        />
+        <div className={classNames('row-span-2 row-start-1', layout === 'portrait-left' ? 'col-start-1' : 'col-start-2')}>
+          <Image image={image3.responsiveImage} />
+        </div>
       )}
     </div>
   </BlockWrapper>
